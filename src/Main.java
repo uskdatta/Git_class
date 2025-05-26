8626007e9298d79a22b08502c388507168f93911
Uppalapati Syam Karni Datta <uppalapati.datta@bounteous.com>
2025-05-26 11:52:11 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 23, 45, 66, 78, 91};
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number + " is even.");
+            } else {
+                System.out.println(number + " is odd.");
+            }
+        }
+    }
+}
